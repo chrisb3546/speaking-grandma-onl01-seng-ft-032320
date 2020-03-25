086ebd5d1a3959eafedfa72b518bf50bad651a45
chris brady <chrisb3546@yahoo.com>
2020-03-25 23:29:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,9 @@
 def speak_to_grandma(words)
   if words != words.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  
-  elsif words == words.upcase
+  elsif words == 'I LOVE YOU GRANDMA!'
+    return "I LOVE YOU TOO PUMPKIN!'"
+  else words == words.upcase
     return "NO, NOT SINCE 1938!"
   
   end 
